ba857bfb236b5992c74536d54fae306b5b493f13
Lliurex <Lliurex@172.16.205.116>
2016-01-18 13:57:48 +0100

    Cambiando estructura de index (OK ENTIDAD Y OK SUCURSAL CREO)
    Merge origin/VILLAR-MAESTRO-DETALLE-CUENTAS-MOVIMIENTOS into SAMUEL-CUENTASYMOVIMIENTOSBANCARIOS
    
    Conflicts:
    	web/index.html

--- src/com/fpmislata/banco/business/domain/SucursalBancaria.java
+++ src/com/fpmislata/banco/business/domain/SucursalBancaria.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.fpmislata.banco.business.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
